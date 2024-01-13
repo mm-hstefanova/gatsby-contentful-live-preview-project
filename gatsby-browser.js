0d7fd6b71575eaa0567224317e0cc4a93fe928d0
mm-hstefanova <hristiyana.stefanova@mentormate.com>
2024-01-13 14:10:11 +0200

    live preview added

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,12 @@ import "@fontsource/dm-sans/500.css"
 import "@fontsource/dm-sans/700.css"
 import "@fontsource/dm-mono"
 import "@fontsource/dm-mono/500.css"
+
+import React from "react"
+import { ContentfulLivePreviewProvider } from "@contentful/live-preview/react"
+
+export const wrapRootElement = ({ element }) => (
+  <ContentfulLivePreviewProvider locale="en-US">
+    {element}
+  </ContentfulLivePreviewProvider>
+)
